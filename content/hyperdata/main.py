305f474b56d699eeb3c82fee14e3e082b0a3b396
FrxHu <fer.szek@gmail.com>
2023-05-13 15:48:22 +0300

    TEst

--- content/hyperdata/main.py
+++ content/hyperdata/main.py
@@ -1,6 +1,8 @@
 import hyperdata.visualize
 import hyperdata.db
 
+%pip install python-dotenv
+
 def plot(stream_name,table_name):
     df = db.read_table(table_name,db.load_credentials())
     visualize.animate(stream_name,df)
