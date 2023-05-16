b8b00d7b14cf55a54b7e4c07c5bab4648c2138b9
FrxHu <fer.szek@gmail.com>
2023-05-16 08:02:30 +0300

    Named visualize import and fixed rest of the namings

--- content/hyperdata/main.py
+++ content/hyperdata/main.py
@@ -1,10 +1,10 @@
-import hyperdata.visualize
+import hyperdata.visualize as visualize
 import hyperdata.db as dbconnector
 
 def plot(stream_name,table_name):
-    df = dbconnector.read_table(table_name,db.load_credentials())
+    df = dbconnector.read_table(table_name,dbconnector.load_credentials())
     visualize.animate(stream_name,df)
 
 def data(stream_name,table_name):
-    df = db.read_table(table_name,db.load_credentials())
+    df = dbconnector.read_table(table_name,db.load_credentials())
     return df
