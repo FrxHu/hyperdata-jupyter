f857a99d8bc13ff13ebde00a1aedc1758c0fc8d5
FrxHu <fer.szek@gmail.com>
2023-05-16 07:40:42 +0300

    Named database connector

--- content/hyperdata/main.py
+++ content/hyperdata/main.py
@@ -1,8 +1,8 @@
 import hyperdata.visualize
-import hyperdata.db
+import hyperdata.db as dbconnector
 
 def plot(stream_name,table_name):
-    df = db.read_table(table_name,db.load_credentials())
+    df = dbconnector.read_table(table_name,db.load_credentials())
     visualize.animate(stream_name,df)
 
 def data(stream_name,table_name):
